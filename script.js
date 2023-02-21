dde68e6bb6593c9366918986c5ede5c0f090814c
Rajpal-26 <rajpaltanwar2608@gmail.com>
2023-02-22 00:48:07 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
+const lastRow = document.createElement("p")
 
+document.body.appendChild(lastRow);
+let total = 0;
 const getSum = () => {
 //Add your code here
-  
+    let price = document.querySelectorAll(".price");
+    // let total = 0;
+    for (let i = 0; i < price.length; i++) {
+        total += parseFloat(price[i].innerHTML);
+    }   
+    console.log(total)
+    // return total;
+    return lastRow.innerHTML = "Total Price: " + total;
+
 };
 
+    
 getSumBtn.addEventListener("click", getSum);
 
